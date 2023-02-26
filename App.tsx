6e2d32878ecf1b3b81e70f9fdbba2576deb72341
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-26 11:50:53 -0600

    Cr8 Header component w/ styling

--- App.tsx
+++ App.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import {SafeAreaView, StyleSheet, View, Text} from 'react-native';
 
 const App = () => {
-  
+
+  const Header = () => {
+    return (
+      <View style={styles.header}>
+        <Text>Header</Text>
+      </View>
+    )
+  }
 
   return (
     <SafeAreaView>
@@ -16,6 +23,11 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+
+  header: {
+    width: '100%',
+    height: '15%',
   }
 });
 
