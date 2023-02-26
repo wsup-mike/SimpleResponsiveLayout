c170a7d6eb079bed18dc4cdf5d8a8b5c43f70981
Mike Ramos <michaelangeloram8@gmail.com>
2023-02-26 12:10:19 -0600

    Add first box to BoxContainer

--- App.tsx
+++ App.tsx
@@ -14,7 +14,9 @@ const App = () => {
   const BoxContainer = () => {
     return (
       <View style={styles.boxContainer}>
-        
+        <View style={styles.box}>
+
+        </View>
       </View>
     )
   }
@@ -40,14 +42,21 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
-  boxContainer: {
+  boxContainer: { // Will house individual boxes inside as additional <Views></View>
     width: '100%',
     backgroundColor: 'red',
     height: '85%',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 5, //padding around each box
+    flexDirection: 'row', // to line up content inside in row fashion horizontally
+    flexWrap: 'wrap', // when content inside is too much for 1 line, then additional ones will wrap to next line underneath
+  },
 
+  box: {
+    
   }
+
 });
 
 export default App;
